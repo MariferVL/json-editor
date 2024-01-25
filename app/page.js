b02aa334f1d9bec77fbbbe01b33c024b4c27166a
Marifer VL <mvillalobos@cadetech.cl>
2024-01-25 17:07:57 -0300

    Add Home component for selecting and editing JSON files

--- app/page.js
+++ app/page.js
@@ -1,112 +1,124 @@
-import Image from "next/image";
+"use client";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { getAvailableGroups, getFilesFromGroup } from './api.js';
 
+/**
+ * Home component for displaying and selecting JSON groups and files.
+ * Fetches available groups and updates the list of files when a group is selected.
+ * Allows users to choose a group and a file for editing.
+ *
+ * @component
+ * @returns {JSX.Element} Home component
+ */
 export default function Home() {
+  // State variables for selected group, selected file, groups, and files in the selected group
+  const [selectedGroup, setSelectedGroup] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [groups, setGroups] = useState([]);
+  const [filesInGroup, setFilesInGroup] = useState([]);
+
+  /**
+   * Fetch available groups when the component mounts.
+   */
+  useEffect(() => {
+    getAvailableGroups().then((response) => {
+      setGroups(response);
+    });
+  }, []);
+
+  /**
+   * Update the list of files when a group is selected.
+   */
+  useEffect(() => {
+    if (selectedGroup) {
+      getFilesFromGroup(selectedGroup).then((files) => {
+        setFilesInGroup(files);
+      });
+    }
+  }, [selectedGroup]);
+
+
+  /**
+   * Generate option elements for a dropdown.
+   * 
+   * @param {string[]} options - Array of options to be included in the dropdown.
+   * @returns {JSX.Element} Options for the dropdown.
+   */
+  const generateOptions = (options) => {
+    return (
+      <>
+        <option value="" disabled>Selecciona una opción</option>
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </>
+    );
+  };
+
+  /**
+   * Handle the selection of a group.
+   * 
+   * @param {string} groupCode - The code of the selected group.
+   */
+  const handleGroupSelection = (groupCode) => {
+    setSelectedGroup(groupCode);
+  };
+
+  /**
+   * Handle the selection of a file.
+   * 
+   * @param {string} fileName - The name of the selected file.
+   */
+  const handleFileSelection = (fileName) => {
+    setSelectedFile(fileName);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Json Editor</h1>
+      <div>
+        <h2>Selecciona un Código:</h2>
+        {/* Dropdown for selecting groups */}
+        <select
+          value={selectedGroup || ''}
+          onChange={(e) => handleGroupSelection(e.target.value)}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          {generateOptions(groups)}
+        </select>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        {selectedGroup && (
+          <>
+            <h2>Selecciona un Archivo para el grupo {selectedGroup}</h2>
+            {/* Dropdown for selecting files */}
+            <select
+              value={selectedFile || ''}
+              onChange={(e) => handleFileSelection(e.target.value)}
+            >
+              {generateOptions(filesInGroup)}
+            </select>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            {/* Buttons to edit with different editors */}
+            {selectedGroup && selectedFile && (
+              <div>
+                <Link
+                  href={`/form-editor/[groupCode]/[fileName]`}
+                  as={`/form-editor/${selectedGroup}/${selectedFile.replace('.json', '')}`}
+                >
+                  Editar con Formulario
+                </Link>
+                <Link
+                  href={`/full-editor/[groupCode]/[fileName]`}
+                  as={`/full-editor/${selectedGroup}/${selectedFile.replace('.json', '')}`}
+                >
+                  Editar con Full Editor
+                </Link>
+              </div>
+            )}
+          </>
+        )}
       </div>
     </main>
   );
